f81cd0c4ee4f1e791ff8ed633456f522809fa3c1
Zhu_shuai <zhu_shuai@hoperun.com>
2019-03-23 22:51:59 +0800

    javap 查看代码，待完善

--- example-demo/src/main/java/com/compile/demo/TryFinallyDemo.java
+++ example-demo/src/main/java/com/compile/demo/TryFinallyDemo.java
@@ -1,16 +1,26 @@
 package com.compile.demo;
 
+/**
+ * 使用javap -verbose TryFinallyDemo.class查看不同
+ * @author sunli
+ *
+ */
 public class TryFinallyDemo {
+	
 	public static void main(String[] args) {
-		// System.out.println(test());
+		String main = "123";
+		System.out.println("test(main)'s result="+test(main));
+		System.out.println("a="+main);
 	}
 	
-	public static String test(){
-		String a = "hello World";
+	public static String test(String param){
+		String a = param;
+		a = "hello";
 		try{
 			return a;
-		} finally{
+		} finally {
 			a = "thank you xiaoyang";
+			System.out.println("test:a="+a);
 		}
 		
 	}
